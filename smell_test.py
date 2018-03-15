d58d184ad77c802f8a1b24cdecd77542244a5e6d
John Saigle <john@computer.local>
2018-03-15 15:38:55 -0400

    Pass pylint with flying colours

--- smell_test.py
+++ smell_test.py
@@ -1,3 +1,11 @@
+"""Smell Test is a script designed to monitor the DNS traffic on a specificied interface and grade
+the TLS of the requested domains.  The relative health of each domain is recorded in JSON files
+and the user will be warned when a domain is seriously vulnerable to a known attack.  The primary
+use envisioned for this software involves sysadmins running this script on their networks and
+notifying domain admins with offending configurations.  In this way, sysadmins can politely
+notify one another when something is misconfigured.
+"""
+
 #!/usr/bin/env python3
 
 import subprocess
@@ -21,9 +29,6 @@ from scapy.all import *
 import xdg.BaseDirectory
 import click
 
-TIMEOUT_IN_SECONDS = 60
-SEVERITY = 'HIGH'
-FILTER_BPF = 'udp and port 53'
 
 # Max size value chosen here is arbitrary. Change it if you want.
 @lru_cache(maxsize=100)
@@ -74,7 +79,7 @@ def cache_path():
         return None
     return path
 
-def generate_report(name, ip_addr):
+def generate_report(name, ip_addr, timeout, severity):
     """Prepare the flags/arguments for testssl and create a subprocess
        calling the testssl.sh executable. Results are written
        to an output file. Format `dir/www.example.com_20180307-164136.json`
@@ -97,7 +102,7 @@ def generate_report(name, ip_addr):
         '--warnings', # testssl.sh will still warn you if there will be a "drastic impact"
         'off',
         '--openssl-timeout', # TODO: instead of timeout, don't run this on HTTP w/out TLS
-        str(timeout_in_seconds),   # Must be a string because it's a command-line argument
+        str(timeout),   # Must be a string because it's a command-line argument
         '--severity',
         severity,
         '--quiet', # leave fewer traces
@@ -107,7 +112,7 @@ def generate_report(name, ip_addr):
         log_path
     ]
     args.append(path_to_executable)
-    for f in flags: args.append(f)
+    for flag in flags: args.append(flag)
     args.append(ip_addr)
 
     """Create testssl fork using subprocess and capture the output. The execept statement will
@@ -124,14 +129,14 @@ def generate_report(name, ip_addr):
     print(output)
 
 # TODO: Create more granular grading criteria
-def grade_https(name, ip):
+def grade_https(name, ip_addr, timeout, severity):
     """Takes as input a website name and its IP and returns a grading.  The grade represents a
     simplified evaluation of SSL/TLS security based on the output of testssl.sh.  Exact criteria
     will be decided later.
     """
     # Generate report and get its path.
-    print('[+] Evaluating {} ({})'.format(name, ip))
-    report_path = generate_report(name, ip)
+    print('[+] Evaluating {} ({})'.format(name, ip_addr))
+    report_path = generate_report(name, ip_addr, timeout, severity)
     if report_path is None: return
 
     # Parse json report file for grade info.
@@ -154,39 +159,45 @@ def grade_https(name, ip):
             print("\t{} vulnerabilities of {} severity".format(summary[key], key))
         print("\tCheck {} for further details".format(report_path))
 
-# this function gets called on all packets that match the sniffer filter
-def select_dns(pkt):
-    """This is called on every packet sniffed by scapy.  After filtering for valid DNS packets,
-    this function extracts the name and IP addr of the DNS requests and passes them along to
-    the `grade_https` function.  The multiprocessing library is used for threading so that the
-    program doesn't hang during grading individual sites.
+def custom_action(timeout, severity):
+    """Acts as a wrapper around `select_dns`.  This is the only way to pass arguments to custom
+    functions using scapy.
+    See [https://thepacketgeek.com/scapy-sniffing-with-custom-actions-part-2/] for more details.
     """
-    # We're only interested in DNS response records
-    if not (DNSRR in pkt and pkt.sport == 53):
-        return
 
-    # Assume DNS records will give us ASCII results. Look into this later.
-    name = pkt[DNSQR].qname.decode("ascii").lower() # The system requested this CNAME.
-    answer = pkt[DNSRR].rdata # Corresponding IPv4 address or alias for the queried name.
+    def select_dns(pkt):
+        """This is called on every packet sniffed by scapy.  After filtering for valid DNS packets,
+        this function extracts the name and IP addr of the DNS requests and passes them along to
+        the `grade_https` function.  The multiprocessing library is used for threading so that the
+        program doesn't hang during grading individual sites.
+        """
+        # We're only interested in DNS response records
+        if not (DNSRR in pkt and pkt.sport == 53):
+            return
 
-    # Sometimes DNS responses come with a trailing period. Normalize the name.
-    if name.endswith('.'):
-        name = name[:-1]
+        # Assume DNS records will give us ASCII results. Look into this later.
+        name = pkt[DNSQR].qname.decode("ascii").lower() # The system requested this CNAME.
+        answer = pkt[DNSRR].rdata # Corresponding IPv4 address or alias for the queried name.
 
-    # Don't evaluate sites when they're in the cache
-    if in_cache(name):
-        return
+        # Sometimes DNS responses come with a trailing period. Normalize the name.
+        if name.endswith('.'):
+            name = name[:-1]
 
-    # Ignore reverse DNS
-    if 'in-addr' in name:
-        return
+        # Don't evaluate sites when they're in the cache
+        if in_cache(name):
+            return
 
-    # Only validate the 'ultimate' DNS result i.e. not aliases
-    if not valid_ip(answer):
-        return
+        # Ignore reverse DNS
+        if 'in-addr' in name:
+            return
 
-    pool = Pool(processes=4)
-    pool.apply_async(grade_https, [name, answer])
+        # Only validate the 'ultimate' DNS result i.e. not aliases
+        if not valid_ip(answer):
+            return
+
+        pool = Pool(processes=4)
+        pool.apply_async(grade_https, [name, answer, timeout, severity])
+    return select_dns
 
 @click.command()
 @click.option(
@@ -203,11 +214,13 @@ def select_dns(pkt):
 @click.argument('interface')
 #@click.options('--interface', help='The network interface to sniff')
 def smell_test(timeout, severity, interface):
-    TIMEOUT_IN_SECONDS = timeout
-    SEVERITY = severity
+    """Invokes the scapy sniff function and filters for DNS requests.  Matching packets are passed
+    to the `select_dns` function for further validating and eventual grading.
+    """
+    filter_bpf = 'udp and port 53'
     print('[**] Beginning "Smell Test"')
     try:
-        sniff(iface=interface, filter=FILTER_BPF, store=0, prn=select_dns)
+        sniff(iface=interface, filter=filter_bpf, store=0, prn=custom_action(timeout,severity))
     except OSError as e:
         # This works on Linux but OS X segfaults when the interface is wrong lmao
         print('[-] ERROR: "{}". (Make sure `interface` matches your network interface)'.format(e))
--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,3 +1,7 @@
+"""This file contains 'misc" tests for the smell_test.py script. This is a first-pass attempt at
+getting some tests up and running; as this project goes, the name of this file should be made more
+descriptive -- or, better yet, should be split into multiple test files grouped by function.
+"""
 import platform
 import os
 
@@ -7,10 +11,14 @@ from smell_test import valid_ip
 from smell_test import cache_path
 
 def test_valid_ip():
+    """Makes sure that ipv4 addresses are accepted and everything else is rejected."""
     assert valid_ip('192.168.0.1') == True
     assert valid_ip('Random garbage') == False
 
 def test_cache_path():
+    """Tests the cache_path on a per-OS basis.  Anything not matchin Linux or Darwin is
+    discarded.
+    """
     this_os = platform.system()
     if this_os == 'Linux':
         assert cache_path() == xdg.BaseDirectory.save_cache_path('smell-test/')
